713c76d1e66334a1ff07d76593b7d9e8e94a6c75
koushik <2k21it29@kiot.ac.in>
2025-04-02 11:13:05 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/BackendPro1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPro1Application.class, args);
+	}
+
+}
